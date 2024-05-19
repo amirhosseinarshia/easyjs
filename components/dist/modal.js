a7af4aee4227b90020a2248815bb364a2ce05018
ossvahid <vbordbar1@gmail.com>
2024-05-19 23:16:52 +0330

    some change on modal

--- components/dist/modal.js
+++ components/dist/modal.js
@@ -4,9 +4,11 @@ export class ESJmodal {
         this.options = {
             animationIn: 'flipInY',
             animationOut: 'flipOutY',
+            animationSpeed: '1s',
             closeByClickModal: true,
             onOpen: () => { },
             onClose: () => { },
+            position: 'center'
         };
         this.options = ESJinit.findEndOptions(this.options, options);
         ESJinit.CheckRequiredOptions([
@@ -26,10 +28,32 @@ export class ESJmodal {
     ComponentUi() {
         const self = this;
         const modal = document.createElement('section');
-        modal.style = 'position: fixed; top: 0; bottom: 0;  left: 0; right: 0; background: rgba(0 , 0 ,0 ,0.8); display: flex; align-items: center; justify-content: center; z-index: 9999999999999;';
+        let position = '';
+        if (self.options.position === 'center' || self.options.position === '') {
+            position = 'align-items: center; justify-content: center;';
+        }
+        else if (self.options.position === 'top-center') {
+            position = 'align-items: flex-start; justify-content: center;';
+        }
+        else if (self.options.position === 'top-left') {
+            position = 'align-items: flex-start; justify-content: flex-start;';
+        }
+        else if (self.options.position === 'top-right') {
+            position = 'align-items: flex-start; justify-content: flex-end;';
+        }
+        else if (self.options.position === 'bottom-center') {
+            position = 'align-items: flex-end; justify-content: center;';
+        }
+        else if (self.options.position === 'bottom-left') {
+            position = 'align-items: flex-end; justify-content: flex-start;';
+        }
+        else if (self.options.position === 'bottom-right') {
+            position = 'align-items: flex-end; justify-content: flex-end;';
+        }
+        modal.style = `position: fixed; top: 0; bottom: 0;  left: 0; right: 0; background: rgba(0 , 0 ,0 ,0.7); display: flex;  z-index: 9999999999999; padding: 20px; ${position}`;
         let popup = document.querySelector(`.${self.options.wrapperClass}`);
         popup = popup.cloneNode(true);
-        popup.style = 'display: block;';
+        popup.style = `display: block; animation-duration: ${self.options.animationSpeed}; `;
         ESJinit.initializeAnimation(popup, `${self.options.animationIn}`);
         document.body.prepend(modal);
         modal.prepend(popup);
--- components/src/modal.ts
+++ components/src/modal.ts
@@ -5,16 +5,20 @@ import { ComponentGlobalOptions, ComponentInterface, ESJinit } from './index.js'
 
 interface ComponentSpecialOptions extends ComponentGlobalOptions {
     closeByClickModal: boolean,
-    removerClass: string
+    removerClass: string,
+    position: string,
+    animationSpeed: string
 }
 
 export class ESJmodal implements ComponentInterface {
     options: Partial<ComponentSpecialOptions> = {
         animationIn: 'flipInY',
         animationOut: 'flipOutY',
+        animationSpeed: '1s',
         closeByClickModal: true,
         onOpen: () => { },
         onClose: () => { },
+        position: 'center'
     };
 
     constructor(options: Partial<ComponentSpecialOptions> = {}) {
@@ -22,7 +26,7 @@ export class ESJmodal implements ComponentInterface {
         ESJinit.CheckRequiredOptions([
             'wrapperClass',
             'handlerClass'
-        ], this.options,'ESJmodal');
+        ], this.options, 'ESJmodal');
         this.ComponentRender();
     }
 
@@ -37,14 +41,30 @@ export class ESJmodal implements ComponentInterface {
     ComponentUi(): void {
         const self = this;
         const modal = document.createElement('section') as any;
-        modal.style = 'position: fixed; top: 0; bottom: 0;  left: 0; right: 0; background: rgba(0 , 0 ,0 ,0.8); display: flex; align-items: center; justify-content: center; z-index: 9999999999999;';
+        let position: string = '';
+        if (self.options.position === 'center' || self.options.position === '') {
+            position = 'align-items: center; justify-content: center;';
+        } else if (self.options.position === 'top-center') {
+            position = 'align-items: flex-start; justify-content: center;';
+        } else if (self.options.position === 'top-left') {
+            position = 'align-items: flex-start; justify-content: flex-start;';
+        } else if (self.options.position === 'top-right') {
+            position = 'align-items: flex-start; justify-content: flex-end;';
+        } else if (self.options.position === 'bottom-center') {
+            position = 'align-items: flex-end; justify-content: center;';
+        } else if (self.options.position === 'bottom-left') {
+            position = 'align-items: flex-end; justify-content: flex-start;';
+        } else if (self.options.position === 'bottom-right') {
+            position = 'align-items: flex-end; justify-content: flex-end;';
+        }
+        modal.style = `position: fixed; top: 0; bottom: 0;  left: 0; right: 0; background: rgba(0 , 0 ,0 ,0.7); display: flex;  z-index: 9999999999999; padding: 20px; ${position}`;
 
         let popup: any = document.querySelector(`.${self.options.wrapperClass}`);
 
 
         popup = popup.cloneNode(true);
 
-        popup.style = 'display: block;';
+        popup.style = `display: block; animation-duration: ${self.options.animationSpeed}; `;
 
 
 
@@ -58,10 +78,13 @@ export class ESJmodal implements ComponentInterface {
             if ((e.target === modal && self.options.closeByClickModal) || e.target === document.querySelector(`.${self.options.removerClass}`)) {
                 ESJinit.initializeAnimation(popup, `${self.options.animationOut}`)
 
-                setTimeout(() => {
-                    document.body.removeChild(modal)
-                   
-                }, 1000);
+            
+
+               setTimeout(() => {
+                document.body.removeChild(modal)
+               }, 1000);
+   
+
                 self.options.onClose();
             }
         }
