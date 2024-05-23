0496132a87c54ee6ed6afada4768a5fa4ec3bfc6
ossvahid <vbordbar1@gmail.com>
2024-05-23 21:10:51 +0330

    completed accordion component

--- components/dist/accordion.js
+++ components/dist/accordion.js
@@ -4,7 +4,8 @@ export class ESJaccordion {
         this.options = {
             allItemOpenable: true,
             toggleSpeed: '0.5s',
-            itemActiveClass: null
+            itemActiveClass: null,
+            icon: 'arrow'
         };
         this.options = ESJinit.findEndOptions(this.options, options);
         ESJinit.CheckRequiredOptions([
@@ -20,10 +21,21 @@ export class ESJaccordion {
     }
     ComponentUi() {
         const self = this;
+        let icon = '';
+        if (self.options.icon === 'arrow') {
+            icon = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="m6 7l6 6l6-6l2 2l-8 8l-8-8z"/></svg>';
+        }
+        else if (self.options.icon === 'plus') {
+            icon = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M18 10h-4V6a2 2 0 0 0-4 0l.071 4H6a2 2 0 0 0 0 4l4.071-.071L10 18a2 2 0 0 0 4 0v-4.071L18 14a2 2 0 0 0 0-4"/></svg>';
+        }
+        else {
+            icon = self.options.icon;
+        }
         document.querySelectorAll(`.${self.options.wrapperClass}`).forEach(function (accordion) {
             accordion.querySelectorAll(`.${self.options.itemHeadingClass}`).forEach((itemHeading, index) => {
                 var _a;
                 let Indx = index + 1;
+                itemHeading.insertAdjacentHTML('beforeend', icon);
                 const accordionItem = (_a = itemHeading.closest(`.${self.options.itemClass}`)) === null || _a === void 0 ? void 0 : _a.querySelector(`.${self.options.itemDataClass}`);
                 if (self.options.defaultItemOpen != Indx) {
                     accordionItem.style = 'display: none;';
@@ -37,6 +49,9 @@ export class ESJaccordion {
                         accordion.querySelectorAll(`.${self.options.itemClass}`).forEach((item) => {
                             item.querySelector(`.${self.options.itemDataClass}`).style = `display: none;`;
                             item.querySelector(`.${self.options.itemDataClass}`).removeAttribute("data-is-open");
+                        });
+                        document.querySelectorAll(`.${self.options.wrapperClass} .${self.options.itemHeadingClass}`).forEach(element => {
+                            element.classList.remove(`${self.options.itemActiveClass}`);
                         });
                     }
                     if (e.target !== accordionItem) {
--- components/src/accordion.ts
+++ components/src/accordion.ts
@@ -8,13 +8,15 @@ interface ComponentSpecialOptions extends ComponentGlobalOptions {
     toggleSpeed: string,
     defaultItemOpen: number,
     itemActiveClass: string | null,
+    icon: string
 
 }
 export class ESJaccordion implements ComponentInterface {
     options: Partial<ComponentSpecialOptions> = {
         allItemOpenable: true,
         toggleSpeed: '0.5s',
-        itemActiveClass: null
+        itemActiveClass: null,
+        icon: 'arrow'
     };
     constructor(options: Partial<ComponentSpecialOptions> = {}) {
         this.options = ESJinit.findEndOptions(this.options, options);
@@ -33,16 +35,25 @@ export class ESJaccordion implements ComponentInterface {
     }
     ComponentUi(): void {
         const self = this;
+        let icon: string | undefined = '';
+        if (self.options.icon === 'arrow') {
+            icon = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="m6 7l6 6l6-6l2 2l-8 8l-8-8z"/></svg>';
+        } else if (self.options.icon === 'plus') {
+            icon = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M18 10h-4V6a2 2 0 0 0-4 0l.071 4H6a2 2 0 0 0 0 4l4.071-.071L10 18a2 2 0 0 0 4 0v-4.071L18 14a2 2 0 0 0 0-4"/></svg>';
+        } else {
+            icon = self.options.icon;
+        }
         document.querySelectorAll(`.${self.options.wrapperClass}`).forEach(function (accordion) {
             (accordion as HTMLElement).querySelectorAll(`.${self.options.itemHeadingClass}`).forEach((itemHeading, index) => {
                 let Indx: number = index + 1;
 
+                itemHeading.insertAdjacentHTML('beforeend', icon as string);
 
                 const accordionItem: any = itemHeading.closest(`.${self.options.itemClass}`)?.querySelector(`.${self.options.itemDataClass}`);
                 if (self.options.defaultItemOpen != Indx) {
                     accordionItem.style = 'display: none;';
-                
-                }else {
+
+                } else {
                     accordionItem.closest(`.${self.options.itemClass}`).querySelector(`.${self.options.itemHeadingClass}`).classList.toggle(`${self.options.itemActiveClass}`)
                 }
 
@@ -55,13 +66,22 @@ export class ESJaccordion implements ComponentInterface {
 
                     if (self.options.allItemOpenable === false) {
                         (accordion as HTMLElement).querySelectorAll(`.${self.options.itemClass}`).forEach((item) => {
-                        
+
                             (item as any).querySelector(`.${self.options.itemDataClass}`).style = `display: none;`;
-                            
+
                             (item as any).querySelector(`.${self.options.itemDataClass}`).removeAttribute("data-is-open");
-                       
+
+
+
                         });
+
+                        document.querySelectorAll(`.${self.options.wrapperClass} .${self.options.itemHeadingClass}`).forEach(element => {
+                            element.classList.remove(`${self.options.itemActiveClass}`);
+                        });
+
                     }
+
+
 
 
 
